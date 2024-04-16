0cab792d3776e148956a34ca069831a3c5eae008
Abhishek Singh <abhisheksinghforsure@gmail.com>
2024-04-16 23:38:07 +0530

    Corrected typo

--- client/src/pages/Pagenotfound.js
+++ client/src/pages/Pagenotfound.js
@@ -7,7 +7,7 @@ const Pagenotfound = () => {
     <Layout title = { 'go back- page not found'}>
       <div className="pnf">
         <h1 className="pnf-title">404</h1>
-        <h2 className="pnf-heading">Oops ! Page Not Found</h2>
+        <h2 className="pnf-heading">Oops! Page Not Found</h2>
         <Link to="/" className="pnf-btn">
           Go Back
         </Link>
